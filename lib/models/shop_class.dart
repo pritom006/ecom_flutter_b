eabd98783e3a76cd36e194367c8ff2c0665724a4
pritom006 <banerjeecse08@gmail.com>
2022-01-15 15:07:11 +0600

    ecom_final_update

---
+++ lib/models/shop_class.dart
@@ -0,0 +1,9 @@
+class ProductId {
+  final String productId;
+  ProductId({required this.productId});
+}
+
+class CategoryName {
+  final String categoryName;
+  CategoryName({required this.categoryName});
+}
